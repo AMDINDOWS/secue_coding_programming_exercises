2c1324f2c6c0889de757c7ede3850223b345f17e
CYBCRYPDAS <30437892+AMDINDOWS@users.noreply.github.com>
2021-10-31 02:00:04 +0530

    Update 02_01_tupled_words.py

--- labs/week4/02_01_tupled_words.py
+++ labs/week4/02_01_tupled_words.py
@@ -5,19 +5,22 @@
 # result_list = [('h', 'e', 'l', 'l', 'o'), ('w', 'o', 'r', 'l', 'd')]
 
 input = "Python is smooth "
+inst=[]
+final=[]
+if type(input)==str:
+    inst=input.split(' ')
 
-for i in input:
-    if i == " ":
-        list1=input.split(" ")
-        for j in list1:
-            if(j==0):
-                tup1=tuple(j)
-                print(tup1) 
-            elif (j==1):
-                tup2=tuple(j)
-                print(tup2) 
-            elif (j== 2):
-                tup3=tuple(j)
-                print(tup3)  
+if inst[-1]=='':
+    inst.pop(-1)
+print(inst)
+for i in (inst):
+    a = tuple(i)
+    final.append(a)
+
+print(final)
+
+
+
+  
                 
             
