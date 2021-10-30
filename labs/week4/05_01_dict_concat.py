f8f7f3a9b44ae32a700222113141921feebf2e34
CYBCRYPDAS <30437892+AMDINDOWS@users.noreply.github.com>
2021-10-31 02:23:01 +0530

    Update 05_01_dict_concat.py

--- labs/week4/05_01_dict_concat.py
+++ labs/week4/05_01_dict_concat.py
@@ -8,3 +8,22 @@
 # dic3={5:50, 6:60}
 # Expected Results: {1: 10, 2: 20, 3: 30, 4: 40, 5: 50, 6: 60}
 
+dic1={1:10, 2:20}
+dic2={3:30, 4:40}
+dic3={5:50, 6:60}
+dic0={}
+lst=[]
+
+""""
+i=0
+while 0<i<=3:
+    dic0.update(dict(i))
+    i+=1
+
+print(dic0)
+"""
+dic0.update(dic1)
+dic0.update(dic2)
+dic0.update(dic3)
+
+print(dic0)
