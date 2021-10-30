d1aa9c08a993e0c3a393d65c5796ace546562c94
CYBCRYPDAS <30437892+AMDINDOWS@users.noreply.github.com>
2021-10-31 01:31:59 +0530

    Update 01_05_list_movezero.py

--- labs/week4/01_05_list_movezero.py
+++ labs/week4/01_05_list_movezero.py
@@ -12,17 +12,21 @@
 list1=[3, 4, 0, 0, 0, 6, 2, 0, 6, 7, 6, 0, 0, 0, 9, 10, 7, 4, 4, 5, 3, 0, 0, 2, 9, 7, 1]
 
 lst2=[]
+lst3=[]
+st=''
 
-for i in list1:
-    if(i==0):
-        lst2.append(i)
-    else:
-        list1.extend(i)
-          
+for i in range(len(list1)):
+    if(list1[i]==0):
+        
+        lst2.append(list1[i])
+    else :    
+        lst3.append(list1[i])
+        
+print(lst3+lst2)
 
-print(lst2)
+#print(lst2)
 
-print(list1)           
+#print(list1)           
 
 
       
