e854635eb40acb23224db2729919610d6efb2f1f
CYBCRYPDAS <30437892+AMDINDOWS@users.noreply.github.com>
2021-10-31 11:24:53 +0530

    Update 05_03_dict_count.py

--- labs/week4/05_03_dict_count.py
+++ labs/week4/05_03_dict_count.py
@@ -4,3 +4,9 @@
 # Track the count of the letters from the string.
 # Sample string : 'securecoding'
 # Expected output: {'s': 1, 'e': 2, 'c': 2, 'u': 1, 'r': 1, 'o': 1, 'd': 1, 'i': 1, 'n': 1, 'g': 1}
+
+st1='securecoding'
+
+dict1={i:st1.count(i) for i in st1 for i in st1}
+print(dict1) 
+
